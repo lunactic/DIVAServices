521686e2f7732c2fe3e13d6b22781103e3a61925
Marcel Würsch <marcel.wuersch@unifr.ch>
2016-11-07 09:29:46 +0100

    added iiifManifestParser

---
+++ src/parsers/iiifManifestParser.ts
@@ -0,0 +1,53 @@
+"use strict";
+/**
+ * Created by lunactic on 07.11.16.
+ */
+let manifesto = require("manifesto.js");
+
+export class IiifManifestParser {
+
+    manifestUrl: string;
+    manifest: any;
+
+    constructor(manifestUrl: string) {
+        this.manifestUrl = manifestUrl;
+    }
+
+    public initialize(): void {
+        let self = this;
+        manifesto.loadManifest(this.manifestUrl).then(function (manifest: any) {
+            self.manifest = manifesto.create(manifest);
+        });
+    }
+
+    public getAllImages(seqIndex: number): any {
+        let images: any = [];
+        let sequence = this.manifest.getSequenceByIndex(seqIndex);
+        let canvases = sequence.getCanvases();
+        for (let canvas of canvases) {
+            images.push(canvas.getImages()[0].getResource().id);
+        }
+        return images;
+    }
+
+    public getMetadata(): any {
+        return this.manifest.getMetadata();
+    }
+
+    public getDescription(): any {
+        return this.manifest.getDescription();
+    }
+
+    public getLabel(): any {
+        return this.manifest.getLabel();
+    }
+
+    public getLicense(): any {
+        return this.manifest.getLicense();
+    }
+
+    public getAttribution(): any {
+        return this.manifest.getAttribution();
+    }
+
+}
