cb2e56627b022a969a28522629a445cd9658254e
Marcel Würsch <marcel.wuersch@unifr.ch>
2017-10-04 11:29:29 +0200

    updated code

--- scripts/startup.docker.sh
+++ scripts/startup.docker.sh
@@ -8,6 +8,7 @@ tsc -p . || true
 logFolder=($(cat conf/server.dev.json | jq -r '.paths.logPath'))
 
 if [ -d "$logFolder" ]; then
+    #NODE_ENV=dev nodemon --max-old-space-size=4096 --inspect=0.0.0.0:9929 ./bin/www --dockerIp=$DOCKERIP --dockerPort=$DOCKERPORT --dockerReport=$DOCKERREPORT --rootIp=$ROOTIP --sshUser=$SSHUSER --sshPass=$SSHPASS
     node --max-old-space-size=4096 ./bin/www --dockerIp=$DOCKERIP --dockerPort=$DOCKERPORT --dockerReport=$DOCKERREPORT --rootIp=$ROOTIP --sshUser=$SSHUSER --sshPass=$SSHPASS
 else
     ./scripts/setup.sh
